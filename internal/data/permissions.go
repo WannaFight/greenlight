e18be3e0bfeda6f36397216110d6ab27bad83bf6
Ivan Trushin <itrushin99@gmail.com>
2024-04-06 13:43:12 +0300

    feat: add permissions

---
+++ internal/data/permissions.go
@@ -0,0 +1,75 @@
+package data
+
+import (
+	"context"
+	"database/sql"
+	"time"
+
+	"github.com/lib/pq"
+)
+
+type Permissions []string
+
+// Include() checks whether the Permissions slice contains a specific permission code.
+func (p Permissions) Include(code string) bool {
+	for i := range p {
+		if code == p[i] {
+			return true
+		}
+	}
+	return false
+}
+
+type PermissionModel struct {
+	DB *sql.DB
+}
+
+// GetAllForUser() returns all permission codes for a specific user in a Permissions slice.
+func (m PermissionModel) GetAllForUser(userID int64) (Permissions, error) {
+	query := `
+		SELECT permissions.code
+		FROM permissions
+		JOIN users_permissions AS us_p ON us_p.permission_id = permissions.id
+		WHERE us_p.user_id = $1`
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	rows, err := m.DB.QueryContext(ctx, query, userID)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var permissions Permissions
+
+	for rows.Next() {
+		var permission string
+
+		err := rows.Scan(&permission)
+		if err != nil {
+			return nil, err
+		}
+
+		permissions = append(permissions, permission)
+	}
+
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return permissions, nil
+}
+
+// AddForUser() adds the provided permission codes for a specific user.
+func (m PermissionModel) AddForUser(userID int64, codes ...string) error {
+	query := `
+		INSERT INTO users_permissions
+		SELECT $1, permissions.id FROM permissions WHERE permissions.code = ANY($2)`
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	_, err := m.DB.ExecContext(ctx, query, userID, pq.Array(codes))
+	return err
+}
---
+++ migrations/000006_add_permissions.up.sql
@@ -0,0 +1,16 @@
+CREATE TABLE IF NOT EXISTS permissions (
+    id bigserial PRIMARY KEY,
+    code text NOT NULL
+);
+
+-- intermediate table for M2M relation
+CREATE TABLE IF NOT EXISTS users_permissions (
+    user_id bigint NOT NULL REFERENCES users ON DELETE CASCADE,
+    permission_id bigint NOT NULL REFERENCES permissions ON DELETE CASCADE,
+    PRIMARY KEY (user_id, permission_id)
+);
+
+INSERT INTO permissions (code)
+VALUES
+    ('movies:read'),
+    ('movies:write');
